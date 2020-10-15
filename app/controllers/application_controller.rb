87f67cb96cf0138b5917091222880ac0c88655a3
TheMartonfi <carlmartonfi@hotmail.com>
2020-10-15 22:37:50 +0000

    Add style to Sign Up page form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,6 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart_subtotal_cents
 
-
   def update_cart(new_cart)
     cookies[:cart] = {
       value: JSON.generate(new_cart),
@@ -37,7 +36,4 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
-
-
-
 end
